579ec4972b2397b1f1e2c0dcb43dd0227fc4b7da
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-01-31 18:16:57 +0530

    Added intervel and reset
    On scroll reset get true and begin again.

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 ScrollReveal().reveal('.section1');
-
-
-ScrollReveal().reveal('.section1');
-
-
 ScrollReveal().reveal('h1', {
-    delay: 1000
+    delay: 1000,
+    reset: true
 });
 ScrollReveal().reveal('h2', {
-    delay: 1500
+    delay: 1500,
 });
-ScrollReveal().reveal('.section2');
-ScrollReveal().reveal('.section3');
-ScrollReveal().reveal('.section4');
+ScrollReveal().reveal('.section2', {
+    reset: true
+});
+ScrollReveal().reveal('.section3', {
+    reset: true
+});
+ScrollReveal().reveal('.section4', {
+    reset: true
+});
+
+
+ScrollReveal().reveal('.little-square', {
+    interval: 40,
+    reset: true
+});
