f7208460d90a83be6ad201715370110186d89f57
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-01-31 17:22:07 +0530

    Initial Commit
    Some simple delay targeting text and div.

---
+++ main.js
@@ -0,0 +1,16 @@
+ScrollReveal().reveal('.section1');
+
+
+ScrollReveal().reveal('.section1');
+ScrollReveal().reveal('.delay1', {
+    delay: 500
+});
+ScrollReveal().reveal('.delay2', {
+    delay: 2000
+});
+ScrollReveal().reveal('.delay3', {
+    delay: 3000
+});
+ScrollReveal().reveal('.section2');
+ScrollReveal().reveal('.section3');
+ScrollReveal().reveal('.section4');
