2088c113ac3cd3f71e426fd6818fea985057a08f
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-01-31 17:47:29 +0530

    Version2
    Solved Flickering Problem.
    Text vertically centerd with addition of span along h1 and h2 tag.

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ ScrollReveal().reveal('.section1');
 
 
 ScrollReveal().reveal('.section1');
-ScrollReveal().reveal('.delay1', {
-    delay: 500
+
+
+ScrollReveal().reveal('h1', {
+    delay: 1000
 });
-ScrollReveal().reveal('.delay2', {
-    delay: 2000
-});
-ScrollReveal().reveal('.delay3', {
-    delay: 3000
+ScrollReveal().reveal('h2', {
+    delay: 1500
 });
 ScrollReveal().reveal('.section2');
 ScrollReveal().reveal('.section3');
